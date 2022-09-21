f47f8bb9382cbb7cbbf87d563f88b01d413f0180
Timur Gayfulin <tiga83@gmail.com>
2022-09-21 12:49:19 +0500

    wip

--- Sources/KitUI/Components/ViewControllers/NavigationBar/BarItem.swift
+++ Sources/KitUI/Components/ViewControllers/NavigationBar/BarItem.swift
@@ -8,52 +8,95 @@ public enum BarItem: Equatable {
 	public static func system(
 		type: UIBarButtonItem.SystemItem,
 		tintColor: UIColor,
-		primaryAction: ViewCommand?,
-		menu: Menu?
+		onTap: ViewCommand?
 	) -> Self {
 		.system(
 			.init(
 				type: type,
 				tintColor: tintColor,
-				primaryAction: primaryAction,
-				menu: menu
+				onTap: onTap
 			)
 		)
 	}
 	
 	public static func regular(
-	title: String?,
-	image: UIImage?,
-	tintColor: UIColor,
-	primaryAction: ViewCommand?,
-	menu: Menu?
+		title: String?,
+		image: UIImage?,
+		tintColor: UIColor,
+		onTap: ViewCommand?
 	) -> Self {
 		.regular(
 			.init(
 				title: title,
 				image: image,
 				tintColor: tintColor,
-				primaryAction: primaryAction,
-				menu: menu
+				onTap: onTap
 			)
 		)
 	}
+	
+//	public static func system(
+//		type: UIBarButtonItem.SystemItem,
+//		tintColor: UIColor,
+//		primaryAction: ViewCommand?,
+//		menu: Menu?
+//	) -> Self {
+//		.system(
+//			.init(
+//				type: type,
+//				tintColor: tintColor,
+//				onTap: primaryAction,
+//				menu: menu
+//			)
+//		)
+//	}
+//
+//	public static func regular(
+//		title: String?,
+//		image: UIImage?,
+//		tintColor: UIColor,
+//		primaryAction: ViewCommand?,
+//		menu: Menu?
+//	) -> Self {
+//		.regular(
+//			.init(
+//				title: title,
+//				image: image,
+//				tintColor: tintColor,
+//				primaryAction: primaryAction,
+//				menu: menu
+//			)
+//		)
+//	}
 }
 
 public struct SystemBarItem: Equatable {
 	public let type: UIBarButtonItem.SystemItem
 	public let tintColor: UIColor
-	public let primaryAction: ViewCommand?
-	public let menu: Menu?
+	public let onTap: ViewCommand?
 }
 
 public struct RegularBarItem: Equatable {
 	public let title: String?
 	public let image: UIImage?
 	public let tintColor: UIColor
-	public let primaryAction: ViewCommand?
-	public let menu: Menu?
+	public let onTap: ViewCommand?
 }
+
+//public struct SystemBarItem: Equatable {
+//	public let type: UIBarButtonItem.SystemItem
+//	public let tintColor: UIColor
+//	public let primaryAction: ViewCommand?
+//	public let menu: Menu?
+//}
+//
+//public struct RegularBarItem: Equatable {
+//	public let title: String?
+//	public let image: UIImage?
+//	public let tintColor: UIColor
+//	public let primaryAction: ViewCommand?
+//	public let menu: Menu?
+//}
 
 extension BarItem {
 	func map() -> UIBarButtonItem {
@@ -66,14 +109,8 @@ extension BarItem {
 	}
 	
 	private func mapSystem(_ item: SystemBarItem) -> UIBarButtonItem {
-		let primaryAction = item.primaryAction.map { command in
-			UIAction { _ in command.perform() }
-		}
-		
 		let button = UIBarButtonItem(
-			systemItem: item.type,
-			primaryAction: primaryAction,
-			menu: item.menu.map { $0.map() }
+			systemItem: item.type
 		)
 		
 		button.tintColor = item.tintColor
@@ -82,19 +119,50 @@ extension BarItem {
 	}
 	
 	private func mapRegular(_ item: RegularBarItem) -> UIBarButtonItem {
-		let primaryAction = item.primaryAction.map { command in
-			UIAction { _ in command.perform() }
-		}
-
 		let button = UIBarButtonItem(
 			title: item.title,
-			image: item.image,
-			primaryAction: primaryAction,
-			menu: item.menu.map { $0.map() }
+			image: item.image
 		)
+		
+		button.isEnabled = item.onTap != nil
 
 		button.tintColor = item.tintColor
 
 		return button
 	}
+	
+//	private func mapSystem(_ item: SystemBarItem) -> UIBarButtonItem {
+//		let primaryAction = item.primaryAction.map { command in
+//			UIAction { _ in command.perform() }
+//		}
+//
+//		let button = UIBarButtonItem(
+//			systemItem: item.type,
+//			primaryAction: primaryAction,
+//			menu: item.menu.map { $0.map() }
+//		)
+//
+//		button.tintColor = item.tintColor
+//
+//		return button
+//	}
+//
+//	private func mapRegular(_ item: RegularBarItem) -> UIBarButtonItem {
+//		let primaryAction = item.primaryAction.map { command in
+//			UIAction { _ in command.perform() }
+//		}
+//
+//		let button = UIBarButtonItem(
+//			title: item.title,
+//			image: item.image,
+//			primaryAction: primaryAction,
+//			menu: item.menu.map { $0.map() }
+//		)
+//
+//		button.isEnabled = primaryAction != nil || item.menu != nil
+//
+//		button.tintColor = item.tintColor
+//
+//		return button
+//	}
 }
